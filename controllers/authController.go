ba1e7740cfb72b9320f05077fef38f0ce10ea299
jylee2 <leej1025y@live.com>
2022-02-02 22:04:12 +0800

    feat(login): find user & check password

--- controllers/authController.go
+++ controllers/authController.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"api-go/database"
+	"api-go/models"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
@@ -65,6 +66,77 @@ func Register(c *fiber.Ctx) error {
   // fmt.Println(insertRes.InsertedID)
   // fmt.Println(user, usersCollection)
   fmt.Println("--------insertRes: ", insertRes)
+
+	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	client, _, _, err := database.Connect("mongodb://localhost:27017")
+  if err != nil {
+		panic(err)
+  }
+
+  usersCollection := client.Database("testing-go").Collection("users")
+
+	// create a search filer
+	// filter := bson.D{
+	// 	{"$and",
+	// 		bson.A{
+	// 			bson.D{
+	// 				{"age", bson.D{{"$gt", 25}}},
+	// 			},
+	// 		},
+	// 	},
+	// }
+	// filter := bson.D{
+	// 	primitive.E{Key: "uuid", Value: uuid.New()},
+	// 	primitive.E{Key: "name", Value: data["name"]},
+	// 	primitive.E{Key: "email", Value: data["email"]},
+	// 	primitive.E{Key: "password", Value: password},
+	// }
+	filter := bson.D{
+		primitive.E{Key: "$and", Value: bson.A{
+			bson.D{
+				primitive.E{Key: "email", Value: data["email"]},
+			},
+		}},
+	}
+  // fmt.Println("--------filter: ", filter)
+
+	// retrieving the first document that match the filter
+	// var user bson.M
+	var user models.User
+	// check for errors in the finding
+	if err = usersCollection.FindOne(context.TODO(), filter).Decode(&user); err != nil {
+		// panic(err)
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "User not found.",
+		})
+	}
+
+	// if user == nil {
+	// 	c.Status(fiber.StatusNotFound)
+	// 	return c.JSON(fiber.Map{
+	// 		"message": "User not found.",
+	// 	})
+  // }
+
+	// display the document retrieved
+	fmt.Println("--------user: ", user)
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Incorrect password.",
+		})
+	}
 
 	return c.JSON(user)
 }
